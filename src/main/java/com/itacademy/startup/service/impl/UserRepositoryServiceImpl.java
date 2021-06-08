f7a6d77f67065f2f65ec130bb7d43a4f2015f653
Elena Piaggio <elenapiaggio@gmail.com>
2021-06-08 13:44:34 +0200

    ADD service implementation

--- src/main/java/com/itacademy/startup/service/impl/UserRepositoryServiceImpl.java
+++ src/main/java/com/itacademy/startup/service/impl/UserRepositoryServiceImpl.java
@@ -1,2 +1,23 @@
-package com.itacademy.startup.service.impl;public class UserRepositoryServiceImpl {
+package com.itacademy.startup.service.impl;
+
+import com.itacademy.startup.entity.UserRepository;
+import com.itacademy.startup.repository.IUserRepositoryRepository;
+import com.itacademy.startup.service.IUserRepositoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserRepositoryServiceImpl implements IUserRepositoryService {
+
+  @Autowired
+  private IUserRepositoryRepository userRepositoryRepository;
+
+
+  @Override
+  public List<UserRepository> getAllUserRepositories() {
+    return userRepositoryRepository.findAll();
+  }
 }
+
