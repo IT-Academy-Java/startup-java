c9f42d2caa101b06f1e7b62c0f643b85032d9cbe
Elena Piaggio <elenapiaggio@gmail.com>
2021-06-08 13:45:05 +0200

    ADD UserRepositoryController

--- src/main/java/com/itacademy/startup/controller/UserRepositoryController.java
+++ src/main/java/com/itacademy/startup/controller/UserRepositoryController.java
@@ -1,2 +1,25 @@
-package com.itacademy.startup.controller;public class UserRepositoryController {
+package com.itacademy.startup.controller;
+
+import com.itacademy.startup.entity.UserRepository;
+import com.itacademy.startup.service.impl.UserRepositoryServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/repositories")
+public class UserRepositoryController {
+
+  @Autowired
+  private UserRepositoryServiceImpl service;
+
+  @GetMapping()
+  @ResponseBody
+  public List<UserRepository> getUserRepositories() throws Exception{
+    return service.getAllUserRepositories();
+  }
 }
