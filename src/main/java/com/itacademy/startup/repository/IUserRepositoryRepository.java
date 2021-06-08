e2165f37427f4c80ccd1be6240a8be0dadd0e838
Elena Piaggio <elenapiaggio@gmail.com>
2021-06-08 13:43:14 +0200

    ADD UserRepository repository

--- src/main/java/com/itacademy/startup/repository/IUserRepositoryRepository.java
+++ src/main/java/com/itacademy/startup/repository/IUserRepositoryRepository.java
@@ -1,2 +1,16 @@
-package com.itacademy.startup.repository;public class IUserRepositoryRepository {
+package com.itacademy.startup.repository;
+
+import com.itacademy.startup.entity.UserRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+@Transactional
+public interface IUserRepositoryRepository extends MongoRepository<UserRepository, Integer> {
+
+  List<UserRepository> findAll();
+
 }
