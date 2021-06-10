87b7cb163425f5ee1da7434e5b0ad7c985262182
Elena Piaggio <elenapiaggio@gmail.com>
2021-06-10 11:29:41 +0200

    ADD DBRef in User classs

--- src/main/java/com/itacademy/startup/entity/User.java
+++ src/main/java/com/itacademy/startup/entity/User.java
@@ -1,71 +1,62 @@
 package com.itacademy.startup.entity;
 
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection="user")
+import java.util.List;
+
+@Document(collection = "user")
 public class User {
-    private String username;
-    private String password;
-    private String email;
-    private int repos;
+  private String username;
+  private String password;
+  private String email;
+  private int repos;
 
-    // @OneToMany
-    //private List<Repo> repos;
+  @DBRef(db = "user")
+  private List<UserRepository> userRepositoryList;
 
-    public User() {
-    }
+  public User() {
+  }
 
-    public User(String username, String password, String email, int repos) {
-        this.username = username;
-        this.password = password;
-        this.email = email;
-        this.repos = repos;
-    }
+  public User(String username, String password, String email, int repos) {
+    this.username = username;
+    this.password = password;
+    this.email = email;
+    this.repos = repos;
+  }
 
-    public String getUsername() {
-        return username;
-    }
+  public String getUsername() {
+    return username;
+  }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+  public void setUsername(String username) {
+    this.username = username;
+  }
 
-    public String getPassword() {
-        return password;
-    }
+  public String getPassword() {
+    return password;
+  }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+  public void setPassword(String password) {
+    this.password = password;
+  }
 
-    public String getEmail() {
-        return email;
-    }
+  public String getEmail() {
+    return email;
+  }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+  public void setEmail(String email) {
+    this.email = email;
+  }
 
-    public int getRepos() {
-        return repos;
-    }
+  public int getRepos() {
+    return repos;
+  }
 
-    public void setRepos(int repos) {
-        this.repos = repos;
-    }
+  public void setRepos(int repos) {
+    this.repos = repos;
+  }
 
-    //@JsonIgnore
-    //@OneToMany(fetch = FetchType.LAZY, mappedBy = "Game")
-
-    /*
-    public List<Repo> getRepos() {
-        return repos;
-    }
-
-    public void setGames(List<Repo> repos) {
-        this.repos = repos;
-    }
-     */
 }
 
 
